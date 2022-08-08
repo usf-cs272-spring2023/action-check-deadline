dfbb5572ce001cf5760aac0f5daf1f316d7ae85d
Sophie Engle <sjengle@cs.usfca.edu>
2022-08-07 23:49:17 -0700

    Adding grade calculation

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const eod = 'T23:59:59';
 const constants = require('./constants.js');
 
 try {
-  // lookup assignment and points possible
+  // lookup assignment
   const assignment_name = core.getInput('assignment_name');
 
   if (!(assignment_name in constants.deadlines)) {
@@ -16,12 +16,7 @@ try {
   }
 
   const assignment = constants.deadlines[assignment_name];
-  const possible = parseInt(assignment.max);
-
   console.log(`Assignment Name: ${assignment_name}`);
-  console.log(`       Possible: ${possible} Points\n`);
-
-  core.setOutput('grade_possible', '${possible}');
 
   // process deadline and possible extension
   let deadline_date = DateTime.fromISO(`${assignment.due}${eod}`, {zone: zone});
@@ -37,37 +32,53 @@ try {
     console.log(`       Extended: ${deadline_text}`);
   }
 
-  core.setOutput('deadline_date', deadline_date.toISO());
-  core.setOutput('deadline_text', deadline_text);
-
   // process submitted date
   const submitted = core.getInput('submitted_date')
   const submitted_date = DateTime.fromISO(submitted).setZone(zone);
-  const submitted_text = submitted.toLocaleString(DateTime.DATETIME_FULL);
+  const submitted_text = submitted_date.toLocaleString(DateTime.DATETIME_FULL);
 
   if (!submitted_date.isValid) {
     throw new Error(`Unable to parse submitted date: ${submitted} (${submitted_date.invalidReason})`);
   }
 
   console.log(`      Submitted: ${submitted_text}\n`);
-  core.setOutput('submitted_date', submitted_date);
-  core.setOutput('submitted_text', submitted_text);
   
+  // process possible and starting points
+  const possible = parseInt(assignment.max);
   const starting_points = parseInt(core.getInput('starting_points'));
 
-  console.log(` Submitted Date: ${submitted_text}`);
-  console.log(`Starting Points: ${starting_points}`);
-  console.log(`Extension Hours: ${extension_hours}`);
+  console.log(`       Possible: ${possible} Points`);
+  console.log(`       Starting: ${starting_points} Points`);
+
+  // calculate late grade
+  let late_interval = 0;
+  let late_multiplier = 0;
+  let late_percent = 0;
+  let late_points = 0;
 
 
+  // calculate earned grade (make sure not negative)
+  let grade_points = Math.max(starting_points - late_points, 0);
+  let grade_percent = grade_points / possible;
 
-  core.setOutput('late_interval', 'hello');
-  core.setOutput('late_multiplier', 'hello');
-  core.setOutput('late_percent', 'hello');
-  core.setOutput('late_points', 'hello');
+  console.log(`   Earned Grade: ${grade_points} Points (${Math.round(grade_percent * 100)})%`);
 
-  core.setOutput('grade_percent', 'hello');
-  core.setOutput('grade_points', 'hello');
+  // set output
+  core.setOutput('grade_possible', `${possible}`);
+
+  core.setOutput('deadline_date', deadline_date.toISO());
+  core.setOutput('deadline_text', deadline_text);
+
+  core.setOutput('submitted_date', submitted_date.toISO());
+  core.setOutput('submitted_text', submitted_text);
+
+  core.setOutput('late_interval',   `${late_interval}`);
+  core.setOutput('late_multiplier', `${late_multiplier}`);
+  core.setOutput('late_percent',    `${late_percent}`);
+  core.setOutput('late_points',     `${late_points}`);
+
+  core.setOutput('grade_percent', `${grade_percent}`);
+  core.setOutput('grade_points', `${grade_points}`);
 } catch (error) {
   core.setFailed(error.message);
 }
