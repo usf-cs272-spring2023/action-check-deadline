017137d370dd55e85db26dbc311cafcb77d4a6b6
Sophie Engle <sjengle@cs.usfca.edu>
2022-08-08 12:27:06 -0700

    Adding annotations

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ try {
   let deadline_text = deadline_date.toLocaleString(DateTime.DATETIME_FULL);
   console.log(`       Deadline: ${deadline_text}`);
 
-  const extension_hours = parseInt(core.getInput('extension_hours'));
+  const extension_hours = parseFloat(core.getInput('extension_hours'));
 
   if (extension_hours > 0) {
     deadline_date = deadline_date.plus({hours: extension_hours}).toISODate();;
@@ -31,6 +31,12 @@ try {
 
     console.log(`       Extended: ${deadline_text}`);
   }
+
+  // TODO Check grade percent works
+  // TODO Check invalid assignment name
+  // TODO Check invalid submitted date
+  // TODO Check 0 or negative points
+  // TODO Add in late calculation
 
   // process submitted date
   const submitted = core.getInput('submitted_date')
@@ -44,8 +50,16 @@ try {
   console.log(`      Submitted: ${submitted_text}\n`);
   
   // process possible and starting points
-  const possible = parseInt(assignment.max);
-  const starting_points = parseInt(core.getInput('starting_points'));
+  const possible = parseFloat(assignment.max);
+  const starting_points = parseFloat(core.getInput('starting_points'));
+
+  if (!(possible > 0)) {
+    throw new Error(`Possible points must be positive: ${possible}`);
+  }
+
+  if (!(starting_points) > 0) {
+    throw new Error(`Starting points must be positive: ${starting_points}`);
+  }
 
   console.log(`       Possible: ${possible} Points`);
   console.log(`       Starting: ${starting_points} Points`);
@@ -59,9 +73,9 @@ try {
 
   // calculate earned grade (make sure not negative)
   let grade_points = Math.max(starting_points - late_points, 0);
-  let grade_percent = grade_points / possible;
+  let grade_percent = Number.parseFloat(grade_points / possible * 100).toFixed(1);
 
-  console.log(`   Earned Grade: ${grade_points} Points (${Math.round(grade_percent * 100)})%`);
+  console.log(`   Earned Grade: ${grade_points} Points (${grade_percent}%)`);
 
   // set output
   core.setOutput('grade_possible', `${possible}`);
@@ -78,7 +92,14 @@ try {
   core.setOutput('late_points',     `${late_points}`);
 
   core.setOutput('grade_percent', `${grade_percent}`);
-  core.setOutput('grade_points', `${grade_points}`);
+  core.setOutput('grade_points',  `${grade_points}`);
+
+  if (late_interval === 0) {
+    core.notice(`${assignment_name}: ${grade_points} / ${possible} (${grade_percent}%)`);
+  }
+  else (late_interval > 0) {
+    core.warning(`${assignment_name}: ${grade_points} / ${possible} (${grade_percent}%)\nLate Penalty: -${late_points} Points`);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
