cb2c4cd7978ad288dca5949ee095a260f44e779f
Sophie Engle <sjengle@cs.usfca.edu>
2022-08-08 13:04:49 -0700

    Adding late logic

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ try {
   }
 
   const assignment = constants.deadlines[assignment_name];
-  console.log(`Assignment Name: ${assignment_name}`);
+  console.log(`Assignment: ${assignment_name}`);
 
   // process deadline and possible extension
   let deadline_date = DateTime.fromISO(`${assignment.due}${eod}`, {zone: zone});
   let deadline_text = deadline_date.toLocaleString(DateTime.DATETIME_FULL);
-  console.log(`       Deadline: ${deadline_text}`);
+  console.log(`  Deadline: ${deadline_text}`);
 
   if (!deadline_date.isValid) {
     throw new Error(`Unable to parse deadline date: ${assignment.due}${eod} (${deadline_date.invalidReason})`);
@@ -33,7 +33,7 @@ try {
     deadline_date = deadline_date.plus({hours: extension_hours}).toISODate();;
     deadline_text = deadline_date.toLocaleString(DateTime.DATETIME_FULL);
 
-    console.log(`       Extended: ${deadline_text}`);
+    console.log(`  Extended: ${deadline_text}`);
   }
 
   // TODO Check grade percent works
@@ -51,41 +51,56 @@ try {
     throw new Error(`Unable to parse submitted date: ${submitted} (${submitted_date.invalidReason})`);
   }
 
-  console.log(`      Submitted: ${submitted_text}\n`);
+  console.log(` Submitted: ${submitted_text}\n`);
   
   // process possible and starting points
-  const possible = parseFloat(assignment.max);
+  const possible_points = parseFloat(assignment.max);
   const starting_points = parseFloat(core.getInput('starting_points'));
 
-  if (!(possible > 0)) {
-    throw new Error(`Possible points must be positive: ${possible}`);
+  if (!(possible_points > 0)) {
+    throw new Error(`Possible points must be positive: ${possible_points}`);
   }
 
   if (!(starting_points) > 0) {
     throw new Error(`Starting points must be positive: ${starting_points}`);
   }
 
-  console.log(`       Possible: ${possible} Points`);
-  console.log(`       Starting: ${starting_points} Points`);
+  console.log(`  Possible: ${possible_points} Points`);
+  console.log(`  Starting: ${starting_points} Points\n`);
 
-  // calculate late grade
-  let late_interval = 0;
+  let late_interval   = 0;
   let late_multiplier = 0;
-  let late_percent = 0;
-  let late_points = 0;
+  let late_percent    = 0;
+  let late_points     = 0;
 
   if (submitted_date > deadline_date) {
+    // calculate late penalties
+    const late_penalty = Number.parseFloat(constants.penalty.percent * possible_points).toFixed(1);
+    const late_maximum = Number.parseFloat(constants.penalty.maximum * possible_points).toFixed(1);
 
+    console.log(`  Penalty: -${late_penalty} Points (-${constants.penalty.percent}%) Per ${constants.penalty.interval} Hours Late`);
+    console.log(`  Maximum: -${late_maximum} Points (-${constants.penalty.maximum}%)\n`);
+
+    // calculate late grade
+    const late_diff = submitted_date.diff(deadline_date, 'hours');
+    late_interval   = late_diff.toObject().hours;
+    late_multiplier = Math.ceil(late_interval / constants.penalty.interval);
+
+    late_points = Math.min(late_maximum, late_multiplier * late_penalty);
+    late_percent = Number.parseFloat(late_points / possible_points * 100).toFixed(1);
+
+    console.log(`  Duration: ${late_interval} Hours Late (x${late_multiplier})`);
+    console.log(` Deduction: -${late_points} Points (-${late_percent}%)\n`);
   }
 
   // calculate earned grade (make sure not negative)
-  let grade_points = Math.max(starting_points - late_points, 0);
-  let grade_percent = Number.parseFloat(grade_points / possible * 100).toFixed(1);
+  let grade_points  = Math.max(starting_points - late_points, 0);
+  let grade_percent = Number.parseFloat(grade_points / possible_points * 100).toFixed(1);
 
-  console.log(`   Earned Grade: ${grade_points} Points (${grade_percent}%)`);
+  console.log(`    Earned: ${grade_points} Points (${grade_percent}%)`);
 
   // set output
-  core.setOutput('grade_possible', `${possible}`);
+  core.setOutput('grade_possible', `${possible_points}`);
 
   core.setOutput('deadline_date', deadline_date.toISO());
   core.setOutput('deadline_text', deadline_text);
@@ -102,10 +117,10 @@ try {
   core.setOutput('grade_points',  `${grade_points}`);
 
   if (late_interval === 0) {
-    core.notice(`${assignment_name}: ${grade_points} / ${possible} (${grade_percent}%)`);
+    core.notice(`${assignment_name}: ${grade_points} / ${possible_points} (${grade_percent}%)`);
   }
   else {
-    core.warning(`${assignment_name}: ${grade_points} / ${possible} (${grade_percent}%)\nLate Penalty: -${late_points} Points`);
+    core.warning(`${assignment_name}: ${grade_points} / ${possible_points} (${grade_percent}%)\nLate Penalty: -${late_points} Points`);
   }
 } catch (error) {
   core.setFailed(error.message);
