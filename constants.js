49855bbed8f6b7f471c154c00f1522e91508efa3
Sophie Engle <sjengle@cs.usfca.edu>
2023-01-30 16:58:14 -0800

    Adding project assignments

--- constants.js
+++ constants.js
@@ -13,6 +13,35 @@ exports.deadlines = {
   'HtmlFetcher':      {'max': 100, 'due': '2023-04-28'},
   'HtmlCleaner':      {'max': 100, 'due': '2023-04-28'},
   'LinkFinder':       {'max': 100, 'due': '2023-05-05'},
+
+  // project 1
+  'Project v1.0 Tests':  {'max': 100, 'due': '2023-02-10'},
+  'Project v1.0 Review': {'max': 100, 'due': '2023-02-17'},
+  'Project v1.1 Tests':  {'max': 100, 'due': '2023-02-17'},
+  'Project v1.1 Review': {'max': 100, 'due': '2023-02-24'},
+  'Project v1.x Design': {'max': 100, 'due': '2023-03-03'},
+
+  // project 2
+  'Project v2.0 Tests':  {'max': 100, 'due': '2023-03-10'},
+  'Project v2.0 Review': {'max': 100, 'due': '2023-03-24'},
+  'Project v2.1 Tests':  {'max': 100, 'due': '2023-03-24'},
+  'Project v2.1 Review': {'max': 100, 'due': '2023-03-31'},
+  'Project v2.x Design': {'max': 100, 'due': '2023-04-10'},
+
+  // project 3
+  'Project v3.0 Tests':  {'max': 100, 'due': '2023-04-10'},
+  'Project v3.0 Review': {'max': 100, 'due': '2023-04-14'},
+  'Project v3.1 Tests':  {'max': 100, 'due': '2023-04-14'},
+  'Project v3.1 Review': {'max': 100, 'due': '2023-04-21'},
+  'Project v3.x Design': {'max': 100, 'due': '2023-04-28'},
+
+  // project 4
+  'Project v4.0 Tests':  {'max': 100, 'due': '2023-05-05'},
+  'Project v4.1 Tests':  {'max': 100, 'due': '2023-05-12'},
+
+  // project 5
+  'Project v5.0 Tests':  {'max': 100, 'due': '2023-05-18'},
+  'Project v5.0 Design': {'max': 100, 'due': '2023-05-18'}
 };
 
 exports.penalty = {
